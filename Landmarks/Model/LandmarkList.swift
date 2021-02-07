36257f83c1f3b2b2c2c04e2d6d1ae714f8944dfc
danielliu1 <dliu1.dev@gmail.com>
2021-02-06 18:05:28 -0800

    Building List and Navigation

---
+++ Landmarks/Model/LandmarkList.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by dl1 on 2/5/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation"))
+        }
+    }
+}

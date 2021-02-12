1b5cdb351bebb4cfe0f10ece08f44a4ea59cc1dc
danielliu1 <dliu1.dev@gmail.com>
2021-02-11 19:57:13 -0800

    Composing Complex Interfaces

---
+++ Landmarks/Categories/CategoryHome.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by dl1 on 2/11/21.
+//
+
+import SwiftUI
+
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List{
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}

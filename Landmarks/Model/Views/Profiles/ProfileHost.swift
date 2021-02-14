6f185e2cd5384970189fee9fd638d2c9af9fdf22
danielliu1 <72581778+danielliu1@users.noreply.github.com>
2021-02-14 09:23:07 -0800

    Working with UI Controls : Introduction

---
+++ Landmarks/Model/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by dl1 on 2/14/21.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}

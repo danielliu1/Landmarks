1dc2fcda1cb794cd36e87caab0f3f992a0865410
danielliu1 <72581778+danielliu1@users.noreply.github.com>
2021-02-15 02:33:42 -0800

    Work with UI Controls, Define the Profile Editor

--- Landmarks/Model/Views/Profiles/ProfileHost.swift
+++ Landmarks/Model/Views/Profiles/ProfileHost.swift
@@ -11,12 +11,16 @@ struct ProfileHost: View {
     @State private var draftProfile = Profile.default
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20 ) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
     }
 }
 
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
--- Landmarks/Model/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Model/Views/Profiles/ProfileSummary.swift
@@ -1,24 +1,47 @@
-//
-//  ProfileSummary.swift
-//  Landmarks
-//
-//  Created by dl1 on 2/14/21.
-//
-
 import SwiftUI
 
 struct ProfileSummary: View {
+
+    @EnvironmentObject var modelData: ModelData
+
     var profile: Profile
-    
     var body: some View {
-        VStack (alignment: .leading, spacing: 10) {
-            Text(profile.username)
-                .bold()
-                .font(.title)
-            
-            Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
-            Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
-            Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+
+                Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+
+                Divider()
+
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+
+                Divider()
+
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    HikeView(hike: modelData.hikes[0])
+                }
+            }
         }
     }
 }
@@ -26,5 +49,6 @@ struct ProfileSummary: View {
 struct ProfileSummary_Previews: PreviewProvider {
     static var previews: some View {
         ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
     }
 }
